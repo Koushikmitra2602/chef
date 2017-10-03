dff6c91e60d7c34a63eadf02d921844960cf764d
root <root@ip-172-31-21-123.ec2.internal>
2017-10-03 21:04:02 +0000

    My firt cookbook

---
+++ README.md
@@ -0,0 +1,4 @@
+# workstations
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: workstations
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
