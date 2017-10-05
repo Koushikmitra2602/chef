57bde8cd8488715815f68aace31211d440113c12
root <root@ip-172-31-21-123.ec2.internal>
2017-10-05 19:20:06 +0000

    extract system data using node object

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,8 +10,21 @@ package 'git' do
   action :install
 end
 
+host=node['ec2']['hostname']
+public_ip=node['ec2']['public_ipv4']
+ip=node['ipaddress']
+cpu=node['cpu']['0']['mhz']
+memory=node['memory']['total']
+
+
 file '/etc/motd' do
-  content 'This server is the property of Koushik Mitra!!'
+  content "This server is the property of Koushik Mitra!!
+HOSTNAME : #{host}
+PUBLIC IPADDRESS : #{public_ip}
+IPADDRESS: #{ip}
+CPU : #{cpu}
+MEMORY : #{memory}
+"
   owner 'root'
   group 'root'
   action :create
