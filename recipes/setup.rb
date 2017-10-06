07eccef80f8c2c35a699b374d70edc04d7936942
root <root@ip-172-31-21-123.ec2.internal>
2017-10-06 18:00:55 +0000

    code refactored using templates

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,21 +10,9 @@ package 'git' do
   action :install
 end
 
-host=node['ec2']['hostname']
-public_ip=node['ec2']['public_ipv4']
-ip=node['ipaddress']
-cpu=node['cpu']['0']['mhz']
-memory=node['memory']['total']
 
-
-file '/etc/motd' do
-  content "This server is the property of Koushik Mitra!!
-HOSTNAME : #{host}
-PUBLIC IPADDRESS : #{public_ip}
-IPADDRESS: #{ip}
-CPU : #{cpu}
-MEMORY : #{memory}
-"
+template '/etc/motd' do
+  source 'motd.erb'
   owner 'root'
   group 'root'
   action :create
